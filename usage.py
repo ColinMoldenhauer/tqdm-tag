30e925462335089f642ee688a91322e2d27613f8
Colin Moldenhauer <colin.moldenhauer@posteo.de>
2026-01-18 18:10:54 +0100

    Example 2

--- usage.py
+++ usage.py
@@ -1,6 +1,25 @@
-from tqdm_color.tqdm_color import ErrorTqdm
+# # ex1: general
+from tqdm_color import tqdm_status
 
-N = 10
-for _ in (pbar := ErrorTqdm(range(N), total=N, colour="green")):
-    if _ == 1: pbar.warn()
-    if _ == 90: pbar.error()
+N = 100
+for _ in (pbar := tqdm_status(
+    range(N),
+    total=N,
+)):
+    if _ == 1: pbar.set_tag("warn", "yellow")
+    if _ == 30: pbar.set_tag("warn")
+    if _ == 90: pbar.set_tag("error", "red")
+
+
+# ex2: update color once done
+import time
+from tqdm_color import tqdm_status
+
+N = 9
+for _ in (pbar := tqdm_status(
+    range(N),
+    total=N,
+    colour="red",
+)):
+    time.sleep(.2)
+    if _ == N-1: pbar.set_tag("default", "green")
