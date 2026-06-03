73b73be882389d250645d7caef1f5fb16645cda6
Colin Moldenhauer <colin.moldenhauer@posteo.de>
2026-06-03 09:51:05 +0200

    Add MathJax support to mkdocs for LaTeX color rendering
    - pymdownx.arithmatex (generic mode) parses $...$ delimiters
    - docs/javascripts/mathjax.js configures MathJax delimiters and
      re-renders math after mkdocs-material instant navigation
    - MathJax CDN loaded via extra_javascript
    
    This enables \color{\texttt{}} rendering on ReadTheDocs,
    matching GitHub's KaTeX-based math renderer.

---
+++ docs/javascripts/mathjax.js
@@ -0,0 +1,20 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true,
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex",
+  },
+};
+
+// Re-render math after mkdocs-material instant navigation
+document$.subscribe(() => {
+  MathJax.startup.output.clearCache();
+  MathJax.typesetClear();
+  MathJax.texReset();
+  MathJax.typesetPromise();
+});
